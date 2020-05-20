9f0d8e94539b9a4f33aa036cf884ddb825f80315
Konstantine Nikka-Sher Piterman <nikka@Konstantines-Mac-mini.local>
2020-05-20 14:16:08 -0700

    second commit

---
+++ ThoughtBlazer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ThoughtBlazer
+//
+//  Created by Konstantine Nikka-Sher Piterman on 5/20/20.
+//  Copyright © 2020 Konstantine Nikka-Sher Piterman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
